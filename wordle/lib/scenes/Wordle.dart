adc98c78a2c2855e744dab8893f33f954871b80e
Rafagcl <rafagonzalezdecanales4+Multimedia@gmail.com>
2023-12-01 21:43:19 +0100

    Update Wordle.dart

--- wordle/lib/scenes/Wordle.dart
+++ wordle/lib/scenes/Wordle.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:wordle/Logica/LogicaWordle.dart';
 
-
 class Wordle extends StatefulWidget {
   @override
   _WordleState createState() => _WordleState();
 }
 
 class _WordleState extends State<Wordle> {
-  WordleGame _wordleGame = WordleGame(); // Instancia de WordleGame para gestionar la lógica del juego
+  WordleGame _wordleGame = WordleGame();
   TextEditingController _guessController = TextEditingController();
+
+  List<List<Color>> _rowColors = List.generate(6, (index) => List.filled(5, Colors.grey));
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +32,7 @@ class _WordleState extends State<Wordle> {
             ),
             ElevatedButton(
               onPressed: () {
+                _updateRowColors();
                 String result = _wordleGame.submitGuess(context, _guessController.text);
                 ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                   content: Text(result),
@@ -39,20 +41,12 @@ class _WordleState extends State<Wordle> {
                 setState(() {});
               },
               child: Text('Hacer Conjetura'),
-
             ),
+            // Mostrar las filas de cuadrados
             for (int i = 0; i < 6; i++)
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(
-                  5,
-                      (index) => Container(
-                    width: 30,
-                    height: 30,
-                    color: Colors.grey,
-                    margin: EdgeInsets.all(5),
-                  ),
-                ),
+                children: _generateRowColors(i),
               ),
             Text('Conjeturas anteriores: ${_wordleGame.previousGuesses.join(', ')}'),
             Text('Intentos restantes: ${_wordleGame.maxAttempts - _wordleGame.previousGuesses.length}'),
@@ -60,5 +54,57 @@ class _WordleState extends State<Wordle> {
         ),
       ),
     );
+  }
+
+  List<Widget> _generateRowColors(int rowIndex) {
+    List<Widget> letterSquares = [];
+
+    for (int i = 0; i < 5; i++) {
+      Color squareColor = _rowColors[rowIndex][i];
+
+      letterSquares.add(Container(
+        width: 30,
+        height: 30,
+        color: squareColor,
+        margin: EdgeInsets.all(5),
+      ));
+    }
+
+    return letterSquares;
+  }
+
+  void _updateRowColors() {
+    // Actualizar solo si la longitud de la palabra introducida es la misma que la palabra objetivo
+    if (_guessController.text.length == _wordleGame.targetWord.length) {
+      for (int i = 0; i < 6; i++) {
+        _rowColors[i] = _getSquareColorsForRow(i);
+      }
+    }
+  }
+
+  List<Color> _getSquareColorsForRow(int rowIndex) {
+    List<Color> colors = [];
+
+    for (int i = 0; i < 5; i++) {
+      Color squareColor = Colors.grey;
+
+      if (i < _wordleGame.targetWord.length) {
+        String targetLetter = _wordleGame.targetWord[i];
+
+        if (i < _guessController.text.length) {
+          String guessedLetter = _guessController.text[i];
+
+          if (guessedLetter == targetLetter) {
+            squareColor = Colors.green;
+          } else if (_wordleGame.targetWord.contains(guessedLetter)) {
+            squareColor = Colors.yellow;
+          }
+        }
+      }
+
+      colors.add(squareColor);
+    }
+
+    return colors;
   }
 }
