9b36f9909dfddf76ee7964a8a820bf374a639225
Rafagcl <rafagonzalezdecanales4+Multimedia@gmail.com>
2023-12-04 09:32:40 +0100

    Teclado arreglado

--- wordle/lib/scenes/Wordle.dart
+++ wordle/lib/scenes/Wordle.dart
@@ -9,6 +9,11 @@ class Wordle extends StatefulWidget {
 class _WordleState extends State<Wordle> {
   WordleGame _wordleGame = WordleGame();
   String _currentInput = '';
+  int contEnteredLetter =0;
+
+  List<LetterSquare> _enteredLetters = List.generate(
+    5, (index) => LetterSquare(Colors.greenAccent, ''),
+  );
 
   List<List<LetterSquare>> letterSquareGrid = List.generate(
     6, (row) =>
@@ -16,6 +21,25 @@ class _WordleState extends State<Wordle> {
         5, (col) => LetterSquare(Colors.grey, ''),
       ),
   );
+  Widget buildEnteredLettersRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: _enteredLetters.map((letterSquare) {
+        return Container(
+          width: 30,
+          height: 30,
+          color: letterSquare.colorL,
+          margin: EdgeInsets.all(5),
+          child: Center(
+            child: Text(
+              letterSquare.letterL,
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+          ),
+        );
+      }).toList(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +51,7 @@ class _WordleState extends State<Wordle> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            buildEnteredLettersRow(),
             Text(
               'Adivina la palabra de 5 letras!',
               style: TextStyle(fontSize: 18),
@@ -81,22 +106,36 @@ class _WordleState extends State<Wordle> {
   }
 
   void handleKey(String key) {
+
     setState(() {
       if (key == 'Delete' && _currentInput.isNotEmpty) {
         _currentInput = _currentInput.substring(0, _currentInput.length - 1);
+        contEnteredLetter --;
+        _updateEnteredLetters(contEnteredLetter, Colors.greenAccent, "");
+
       } else if (key == 'Submit' && _currentInput.length == 5) {
         _wordleGame.submitGuess(context, _currentInput);
         _updateSquares();
+        contEnteredLetter=0;
+        for(int i=0; i<5; i++){
+          _updateEnteredLetters(i, Colors.greenAccent, "");
 
+        }
         _currentInput = '';
       } else if (key != 'Delete' && key != 'Submit' && _currentInput.length < 5) {
         _currentInput += key;
+        _updateEnteredLetters(contEnteredLetter, Colors.greenAccent, key);
+        contEnteredLetter ++;
         //_updateSquares();
       }
     });
   }
 
-
+  void _updateEnteredLetters(int i, Color newColor, String key){
+    setState(() {
+      _enteredLetters[i] = LetterSquare(newColor, key);
+    });
+  }
   void _updateSquares() {
     int currentAttemptIndex = _wordleGame.currentAttempt -1;
 
