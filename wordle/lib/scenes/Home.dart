698b55cf7bb0abb7e0d9a1abb6dbd6ac63fd396f
LucasAH9 <lucasandosilla@gmail.com>
2023-12-04 21:05:24 +0100

    añadidos botones en menú principal
    nuevos botones y texto en menu principal manteniendo la funcionalidad previa

--- wordle/lib/scenes/Home.dart
+++ wordle/lib/scenes/Home.dart
@@ -6,7 +6,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Wordle Game'),
+        //title: Text('Wordle Game'),
       ),
       body: Center(
         child: Column(
@@ -20,38 +20,79 @@ class Home extends StatelessWidget {
               style: TextStyle(fontSize: 20),
             ),*/
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Navegar a la página de juego cuando se presiona el botón
-                Navigator.pushNamed(
-                  context,
-                  '/wordle', // Usa Wordle en lugar de Worldle
-                  arguments: {'lista': 0}
-                );
-              },
-              child: Text('Tematica: Comida'),
+            Image.asset(
+              'assets/images/elige_una_tematica.PNG',
             ),
-            ElevatedButton(
-             onPressed: () {
-               // Navegar a la página de juego cuando se presiona el botón
-               Navigator.pushNamed(
-                    context,
-                    '/wordle', // Usa Wordle en lugar de Worldle
-                    arguments: {'lista': 1}
-                );
-              },
-              child: Text('Tematica: Animales'),
-            ),
-            ElevatedButton(
-             onPressed: () {
-                // Navegar a la página de juego cuando se presiona el botón
+            /*Text(
+              'Bienvenido a Wordle!',
+              style: TextStyle(fontSize: 20),
+            ),*/
+            SizedBox(height: 15),
+
+            GestureDetector(
+              onTap:(){
                 Navigator.pushNamed(
                     context,
                     '/wordle', // Usa Wordle en lugar de Worldle
-                    arguments: {'lista': 2}
+                    arguments: {'lista': 0}
                 );
               },
-              child: Text('Tematica: Océano'),
+              child:Column (
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 200, // Ancho deseado
+                     child: Image.asset(
+                       'assets/images/comida.PNG', // Ruta de la imagen
+                      fit: BoxFit.contain, // Ajuste de la imagen para que quepa en el contenedor
+                     ),
+                  )
+                ],
+              )
+            ),
+            SizedBox(height: 10),
+            GestureDetector(
+                onTap:(){
+                  Navigator.pushNamed(
+                      context,
+                      '/wordle', // Usa Wordle en lugar de Worldle
+                      arguments: {'lista': 1}
+                  );
+                },
+                child:Column (
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 200, // Ancho deseado
+                      child: Image.asset(
+                        'assets/images/animales.PNG', // Ruta de la imagen
+                        fit: BoxFit.contain, // Ajuste de la imagen para que quepa en el contenedor
+                      ),
+                    )
+                  ],
+                )
+            ),
+            SizedBox(height: 10),
+            GestureDetector(
+                onTap:(){
+                  Navigator.pushNamed(
+                      context,
+                      '/wordle', // Usa Wordle en lugar de Worldle
+                      arguments: {'lista': 2}
+                  );
+                },
+                child:Column (
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 200, // Ancho deseado
+                      child: Image.asset(
+                        'assets/images/oceano.PNG', // Ruta de la imagen
+                        fit: BoxFit.contain, // Ajuste de la imagen para que quepa en el contenedor
+                      ),
+                    )
+                  ],
+                )
             ),
           ],
         ),
